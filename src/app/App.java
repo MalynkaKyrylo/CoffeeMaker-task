38f926d6e92caecda30725bcf81a536e2043bc2c
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-09-05 01:47:47 +0300

    Solution App start

--- src/app/App.java
+++ src/app/App.java
@@ -1,18 +1,11 @@
 package app;
 
-import app.drinks.CoffeeMaker;
-import app.drinks.Espresso;
+import app.utils.AppView;
+import app.utils.AppController;
 
 public class App {
     public static void main(String[] args) {
-        CoffeeMaker coffeeMaker = new CoffeeMaker();
-
-        coffeeMaker.setDrink
-            chooseOption();
-        );
-
-        coffeeMaker
-
+        AppController.filterChoice(AppView.chooseOption());
 
     }
 }
